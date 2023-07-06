48bf7ce5234c16e282630c450fc4fabc3dfb4294
mathews-hos <129777645+mathews-hos@users.noreply.github.com>
2023-07-06 17:35:46 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,22 @@
 import 'package:flutter/material.dart';
+import 'telas/cadastro.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const MyApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'formulários',
-      darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: const Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  //variáveis de estado
-  final _formkey = GlobalKey<FormState>();
-  final TextEditingController _titulocontroller = TextEditingController();
-  final TextEditingController _duracaocontroller = TextEditingController();
-  final TextEditingController _anocontroller = TextEditingController();
-  final TextEditingController _generocontroller = TextEditingController();
-  final TextEditingController _artistacontroller = TextEditingController();
-  final TextEditingController _albumcontroller = TextEditingController();
-  // fim 👍
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("formulei"),
-        backgroundColor: Colors.blue.shade900,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(
+        useMaterial3: true,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Form(
-              key: _formkey,
-              child: Column(children: [
-                const Text("cadastro de música"),
-                // título
-                TextFormField(
-                  controller: _titulocontroller,
-                  decoration: const InputDecoration(
-                      labelText: "Nome",
-                      prefixIcon: Icon(Icons.audiotrack_rounded)),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "obrigatório";
-                    }
-                    return null;
-                  },
-                ),
-                // duração
-                TextFormField(
-                  controller: _duracaocontroller,
-                  decoration: const InputDecoration(labelText: "Duração"),
-                ),
-                // ano lançamento
-                TextFormField(
-                  controller: _anocontroller,
-                  decoration:
-                      const InputDecoration(labelText: "Ano de lançamento"),
-                ),
-                // Gênero
-                TextFormField(
-                  controller: _generocontroller,
-                  decoration: const InputDecoration(labelText: "Gênero"),
-                ),
-                // artista
-                TextFormField(
-                  controller: _artistacontroller,
-                  decoration:
-                      const InputDecoration(labelText: "Artista da música"),
-                ),
-                // album
-                TextFormField(
-                  controller: _albumcontroller,
-                  decoration: const InputDecoration(labelText: "Álbum"),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      if (_formkey.currentState!.validate()) {
-                        print("Música cadastrada");
-                        print('${_titulocontroller.text}aa');
-                        print('${_duracaocontroller.text}');
-                        print('${_anocontroller.text}');
-                        print('${_generocontroller.text}');
-                        print('${_artistacontroller.text}');
-                        print('${_albumcontroller.text}');
-                      }
-                    },
-                    child: Text("yey"))
-              ]),
-            ),
-          ),
-        ),
-      ),
+      home: const Porfil(title: 'cadastro curso'),
     );
   }
 }
