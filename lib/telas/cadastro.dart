c0755a7d58e03b99f49b1b1d434cc741051606d3
mathews-hos <mathewshosoares@gmail.com>
2023-07-07 15:38:51 -0300

    validação quase pronta

--- lib/telas/cadastro.dart
+++ lib/telas/cadastro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Porfil extends StatefulWidget {
   const Porfil({super.key, required this.title});
@@ -10,6 +11,7 @@ class Porfil extends StatefulWidget {
 }
 
 class _PorfilState extends State<Porfil> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _desccurso = TextEditingController();
   final TextEditingController _cargahoracurso = TextEditingController();
   final TextEditingController _modalidade =
@@ -28,102 +30,131 @@ class _PorfilState extends State<Porfil> {
       body: Container(
           padding: const EdgeInsets.all(10),
           child: Center(
-            child: Column(
-              children: [
-                Text(
-                  "Cadastro de curso",
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                TextFormField(
-                  controller: _desccurso,
-                  decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(color: Colors.blue)),
-                      labelText: "descrição",
-                      prefixIcon: const Icon(Icons.text_snippet)),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 7),
-                ),
-                TextFormField(
-                  controller: _cargahoracurso,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      labelText: "cargahorária",
-                      border: const OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(color: Colors.blue)),
-                      prefixIcon: const Icon(Icons.av_timer)),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(top: 7),
-                ),
-
-                DropdownButton(
-                  value: _modalidade.text,
-                  items: const [
-                    DropdownMenuItem(
-                      value: "escolha um",
-                      child: Text("escolha um"),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Text(
+                      "Cadastro de curso",
+                      style: Theme.of(context).textTheme.headlineMedium,
                     ),
-                    DropdownMenuItem(
-                        value: "Qualificação", child: Text("Qualificação")),
-                    DropdownMenuItem(
-                        value: "Aperfeiçoamento",
-                        child: Text("Aperfeiçoamento")),
-                    DropdownMenuItem(value: "Técnico", child: Text("Técnico")),
-                    DropdownMenuItem(
-                        value: "Graduação", child: Text("Graduação")),
-                    DropdownMenuItem(
-                        value: "Pós-Graduação", child: Text("Pós-Graduação")),
-                  ],
-                  onChanged: (a) {
-                    setState(() {
-                      _modalidade.text = a!;
-                      // ignore: avoid_print
-                      print(a);
-                    });
-                  },
-                  autofocus: true,
-                ),
+                    TextFormField(
+                      validator: (valor) {
+                        if (valor!.isEmpty) {
+                          return "insira a descrição do curso";
+                        }
+                        return null;
+                      },
+                      controller: _desccurso,
+                      decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(color: Colors.blue)),
+                          labelText: "descrição",
+                          prefixIcon: const Icon(Icons.text_snippet)),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 7),
+                    ),
+                    TextFormField(
+                      validator: (valor) {
+                        if (valor!.isEmpty) {
+                          return "insira a carga horária do curso";
+                        }
+                        return null;
+                      },
+                      controller: _cargahoracurso,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          labelText: "cargahorária",
+                          border: const OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(color: Colors.blue)),
+                          prefixIcon: const Icon(Icons.av_timer)),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 7),
+                    ),
 
-                const Padding(
-                  padding: EdgeInsets.only(top: 7),
-                ),
-                TextFormField(
-                  validator: (valor) {
-                    if (valor!.isEmpty) {
-                      return "a";
-                    }
-                    return null;
-                  },
-                  controller: _eixo,
-                  decoration: InputDecoration(
-                      labelText: "eixo",
-                      border: const OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          borderSide: const BorderSide(color: Colors.blue)),
-                      prefixIcon: const Icon(Icons.widgets_outlined)),
-                ),
-                ElevatedButton(
-                    onPressed: () {}, child: const Icon(Icons.add_box_rounded))
-                // DropdownButton(
-                //   items: const [
-                //     DropdownMenuItem(child: Text("data")),
-                //     DropdownMenuItem(child: Text("a"))
-                //   ],
-                //   onChanged: (abelha) {
-                //     setState(() {
-                //       print("object");
-                //     });
-                //   },
-                // )
-              ],
-            ),
+                    DropdownButtonFormField(
+                      validator: (value) {
+                        if (value == "escolha um") {
+                          return "escolha uma modalidade";
+                        }
+                        return null;
+                      },
+                      value: _modalidade.text,
+                      items: const [
+                        DropdownMenuItem(
+                          value: "escolha um",
+                          child: Text("escolha um"),
+                        ),
+                        DropdownMenuItem(
+                            value: "Qualificação", child: Text("Qualificação")),
+                        DropdownMenuItem(
+                            value: "Aperfeiçoamento",
+                            child: Text("Aperfeiçoamento")),
+                        DropdownMenuItem(
+                            value: "Técnico", child: Text("Técnico")),
+                        DropdownMenuItem(
+                            value: "Graduação", child: Text("Graduação")),
+                        DropdownMenuItem(
+                            value: "Pós-Graduação",
+                            child: Text("Pós-Graduação")),
+                      ],
+                      onChanged: (a) {
+                        setState(() {
+                          _modalidade.text = a!;
+                          // ignore: avoid_print
+                          print(a);
+                        });
+                      },
+                    ),
+
+                    const Padding(
+                      padding: EdgeInsets.only(top: 7),
+                    ),
+                    TextFormField(
+                      validator: (valor) {
+                        if (valor!.isEmpty) {
+                          return "escolha um eixo";
+                        }
+                        return null;
+                      },
+                      controller: _eixo,
+                      decoration: InputDecoration(
+                          labelText: "eixo",
+                          border: const OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30),
+                              borderSide: const BorderSide(color: Colors.blue)),
+                          prefixIcon: const Icon(Icons.widgets_outlined)),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            Fluttertoast.showToast(
+                                msg: "a",
+                                gravity: ToastGravity.CENTER_LEFT,
+                                backgroundColor: Colors.green);
+                          }
+                        },
+                        child: const Icon(Icons.add_box_rounded))
+                    // DropdownButton(
+                    //   items: const [
+                    //     DropdownMenuItem(child: Text("data")),
+                    //     DropdownMenuItem(child: Text("a"))
+                    //   ],
+                    //   onChanged: (abelha) {
+                    //     setState(() {
+                    //       print("object");
+                    //     });
+                    //   },
+                    // )
+                  ],
+                )),
           )),
     );
   }
